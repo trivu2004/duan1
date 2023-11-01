9be8adf9006ae70a5bdc68a259ae329c8fbd1c46
tridung778 <crytv778@gmail.com>
2023-11-01 19:11:11 +0700

    test SQL

---
+++ src/Form/testSQL.java
@@ -0,0 +1,35 @@
+package Form;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Tri Dung
+ */
+public class testSQL {
+    public static void main(String[] args) {
+        // Thông tin kết nối MySQL
+        String url = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12658501";
+        String username = "sql12658501";
+        String password = "ap63uLZLNJ";
+
+        // Thử kết nối
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            // Thêm code thực hiện các thao tác với cơ sở dữ liệu ở đây
+            Statement st = connection.createStatement();
+            String sql = "Select * from NhanVien";
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()) {
+                System.out.println(rs.getString("MaNV"));
+            }
+            connection.close(); // Đừng quên đóng kết nối sau khi sử dụng xong
+        } catch (SQLException e) {
+            System.err.println("Lỗi kết nối MySQL: " + e.getMessage());
+        }
+    }
+}
