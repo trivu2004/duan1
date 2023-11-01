6bca1da34027d501f079add8055a3f9f11db8463
trivu2004 <kahuka2001@gmail.com>
2023-11-01 09:40:35 +0700

    DuAn1

---
+++ src/duan1/DuAn1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package duan1;
+
+/**
+ *
+ * @author USER
+ */
+public class DuAn1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("duan1.DuAn1.main()");
+    }
+    
+}
