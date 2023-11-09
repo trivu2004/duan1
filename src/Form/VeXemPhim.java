2794333523cfdc0b89dad6fa120bc5ba627bf57c
tridung778 <crytv778@gmail.com>
2023-11-09 14:34:21 +0700

    tao ve

--- src/Form/VeXemPhim.java
+++ src/Form/VeXemPhim.java
@@ -19,31 +19,31 @@ import java.util.Date;
 
 public class VeXemPhim {
 
-    public void inVeXemPhimPDF(String tenTep, String maQRCode, String nhanVien, String tenPhim, String suatChieu, String phongChieu, String viTriGhe, double tongTien) {
+    public void inVeXemPhimPDF(String tenTep, String nhanVien, String tenPhim, String suatChieu, String phongChieu, String viTriGhe, double tongTien) {
         try {
             WriterProperties writerProperties = new WriterProperties();
             writerProperties.setCompressionLevel(CompressionConstants.BEST_COMPRESSION);
 
-            PdfWriter pdfWriter = new PdfWriter(tenTep);
+            PdfWriter pdfWriter = new PdfWriter("src\\Ve\\" + tenTep+".pdf");
             PdfDocument pdfDocument = new PdfDocument(pdfWriter);
             pdfDocument.setDefaultPageSize(PageSize.A6);
             Document document = new Document(pdfDocument);
-            document.setMargins(0, 0, 0, 0);
+            document.setMargins(0, 10, 0, 10);
 
             SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-            document.add(new Paragraph("CINEMAX QUAN 12").setBold().setFontSize(20).setTextAlignment(TextAlignment.CENTER));
+            document.add(new Paragraph("CINEMAX").setBold().setFontSize(20).setTextAlignment(TextAlignment.CENTER));
             document.add(new Paragraph("VE XEM PHIM").setFontSize(14).setTextAlignment(TextAlignment.CENTER));
-            document.add(new Paragraph("--------------------------------------------------------"));
+            document.add(new Paragraph("---------------------------------------------------------------------"));
             document.add(new Paragraph("Thoi gian tao ve: " + dateFormat.format(new Date())));
             document.add(new Paragraph("Nhan Vien: " + nhanVien));
-            document.add(new Paragraph("--------------------------------------------------------"));
+            document.add(new Paragraph("---------------------------------------------------------------------"));
             document.add(new Paragraph("Phim: " + tenPhim));
             document.add(new Paragraph("Suat: " + suatChieu));
             document.add(new Paragraph("Phong: " + phongChieu + "    " + "Ghe: " + viTriGhe));
-            document.add(new Paragraph("==============================="));
+            document.add(new Paragraph("======================================="));
             document.add(new Paragraph("Tong tien: " + tongTien + " VND").setFontSize(12).setTextAlignment(TextAlignment.RIGHT));
-            document.add(new Paragraph("--------------------------------------------------------"));
-            BarcodeQRCode qrCode = new BarcodeQRCode(maQRCode);
+            document.add(new Paragraph("---------------------------------------------------------------------"));
+            BarcodeQRCode qrCode = new BarcodeQRCode(tenTep);
             PdfFormXObject qrCodeObject = qrCode.createFormXObject(pdfDocument);
             Image image = new Image(qrCodeObject).setWidth(80).setHorizontalAlignment(HorizontalAlignment.CENTER);
             document.add(image);
@@ -56,6 +56,6 @@ public class VeXemPhim {
 
     public static void main(String[] args) {
         VeXemPhim veXemPhim = new VeXemPhim();
-        veXemPhim.inVeXemPhimPDF("ve_xem_phim.pdf", "VE001", "nv001", "sex  gay", "10am - 11am", "603", "f1", 10000);
+        veXemPhim.inVeXemPhimPDF("Ve_ID", "nv001", "sex  gay", "10am - 11am", "603", "f1", 10000);
     }
 }
