7c2eb4c95c0c80127dbbe8ae13952a3400b7b137
trivu2004 <kahuka2001@gmail.com>
2023-11-18 21:48:31 +0700

    a

--- src/Form/MainJFrame.java
+++ src/Form/MainJFrame.java
@@ -74,14 +74,11 @@ public class MainJFrame extends javax.swing.JFrame {
                     String MatKhau = kq.getString("MatKhau");
                     tenNhanVien = kq.getString("TenNhanVien");
                     // Mã hóa mật khẩu
-                    if (BCryptPasswordHashing.verifyPassword(loginForm.txtPass.getText().trim(), MatKhau)&NhanVienID.equals(loginForm.txtUser.getText())) {
-<<<<<<< HEAD
-//                    
+                    if (BCryptPasswordHashing.verifyPassword(loginForm.txtPass.getText().trim(), MatKhau)&NhanVienID.equals(loginForm.txtUser.getText())) {      
                     if (BCryptPasswordHashing.verifyPassword(loginForm.txtPass.getText().trim(), MatKhau) & NhanVienID.equals(loginForm.txtUser.getText().trim())) {
 
                     if (BCryptPasswordHashing.verifyPassword(loginForm.txtPass.getText().trim(), MatKhau) & NhanVienID.equals(loginForm.txtUser.getText())) {
-=======
->>>>>>> af348e5c117ad910cac8178c7a1f32b1f74cdd8b
+
                         FlatAnimatedLafChange.showSnapshot();
                         app.setContentPane(app.mainForm);
                         app.mainForm.applyComponentOrientation(app.getComponentOrientation());
@@ -94,11 +91,8 @@ public class MainJFrame extends javax.swing.JFrame {
                     }
                     Notifications.getInstance().show(Notifications.Type.INFO, Notifications.Location.TOP_CENTER, "Sai tài khoản hoặc mật khẩu !");
                 }
-<<<<<<< HEAD
                 }
                 }
-=======
->>>>>>> af348e5c117ad910cac8178c7a1f32b1f74cdd8b
             } catch (Exception e) {
             }
         } else {
